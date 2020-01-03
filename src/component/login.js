edfda3f3308e80497ba369295bc83e8f46d38344
minhajspace <minhaj.space@gmail.com>
2020-01-03 20:30:53 +0530

    login page done

--- src/component/login.js
+++ src/component/login.js
@@ -28,30 +28,18 @@ class Login extends React.Component {
                         <input
                             type="email"
                             required
-                            name=""
-                            placeholder="Enter username"
-                            value={userName}
-                            onChange={e =>
-                                this.setState({ userName: e.target.value });
 
-                        }
-                    />
-                    <label for="psw">Password</label>
+                            placeholder="Enter username"
+
+                        />
+                        <label for="psw">Password</label>
                         <input
                             type="password"
                             required
                             placeholder="Enter password"
-                            id="psw"
-                            name="psw"
-                            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                            title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
-                            value={password}
-                            onChange={event =>
-                                this.setState({ password: event.target.value });
 
-                        }
-                    />
-                    <input type="submit" onClick={this.onUserSubmit} />
+                        />
+                        <input type="submit" onClick={this.onUserSubmit} />
                         <a href="#">Lost your Password ? </a> <br />
                         <a href="#">Don't have an account?</a>
                     </form>
