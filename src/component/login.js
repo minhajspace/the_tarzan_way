158eb172983101062535d887ea80e55e37cf58f1
minhajspace <minhaj.space@gmail.com>
2020-01-03 21:05:17 +0530

    user validation complete

--- src/component/login.js
+++ src/component/login.js
@@ -1,17 +1,36 @@
 import React from "react";
 import './login.css'
 
+const soldier = {
+    email: "soldier@thetarzanway.com",
+    password: "soldier"
+
+}
+const Terrorist = {
+    email: "terrorist@thetarzanway.com",
+    password: 'terrorist'
+
+}
+
 
 class Login extends React.Component {
     state = {
         userName: "",
         password: ''
     }
-    onUserSubmit = (e) => {
-        e.preventDefault()
+    onCheckUserValid = (e) => {
+        e.preventDefault();
+        const { userName, password } = this.state
+
+        if (userName == soldier.email && password == soldier.password) { console.log("you are aurthroized person "); console.log(userName, password); }
+
+
+        else if (userName == Terrorist.email && password == Terrorist.password) { console.log("you are aurthroized person "); console.log(userName, password) }
+        else console.log("your cant login  ")
+
+
 
     }
-
     render() {
         return (
 
@@ -23,13 +42,14 @@ class Login extends React.Component {
                         src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS3Nw8Rc-b0VL9GkaoIuIYMKNI6b6qBQ-PwTHn2gZN2lld822Gp"
                     />
                     <h1>Login Here </h1>
-                    <form ref={this.form}>
-                        <lable>Username</lable>
+                    <form >
+                        <lable>Email</lable>
                         <input
                             type="email"
                             required
-
+                            value={this.state.userName}
                             placeholder="Enter username"
+                            onChange={(e) => { this.setState({ userName: e.target.value }) }}
 
                         />
                         <label for="psw">Password</label>
@@ -37,9 +57,11 @@ class Login extends React.Component {
                             type="password"
                             required
                             placeholder="Enter password"
+                            value={this.state.password}
+                            onChange={(e) => { this.setState({ password: e.target.value }) }}
 
                         />
-                        <input type="submit" onClick={this.onUserSubmit} />
+                        <input type="submit" onClick={this.onCheckUserValid} />
                         <a href="#">Lost your Password ? </a> <br />
                         <a href="#">Don't have an account?</a>
                     </form>
